a002457cfd3c6908e041986d3892f81fdf24cdd6
Patrick Brennan <pbrennanmusic@gmail.com>
2021-09-22 22:56:56 -0400

    commit tree b3fb460b0928b1b21817e9dd42db732b0a52185a

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const element = document.querySelector('main#main');
+element.remove();
+
+//document.body.innerHTML = '<h1 id=\"victory\"></h1>';
+const h1 = document.createElement('h1');
+h1.setAttribute('id', 'victory');
+document.body.appendChild(h1);
+const newHeader = document.querySelector('h1#victory');
+newHeader.innerHTML = '\"YOUR-NAME is the champion\"';
