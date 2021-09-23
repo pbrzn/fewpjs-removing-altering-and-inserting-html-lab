f82f7b36a2d64752f3d5315919ec1d52640e0c9f
Patrick Brennan <pbrennanmusic@gmail.com>
2021-09-22 22:57:29 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const element = document.querySelector('main#main');
+element.remove();
+
+const h1 = document.createElement('h1');
+h1.setAttribute('id', 'victory');
+document.body.appendChild(h1);
+const newHeader = document.querySelector('h1#victory');
+newHeader.innerHTML = '\"YOUR-NAME is the champion\"';
